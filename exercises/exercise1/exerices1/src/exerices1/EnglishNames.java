06ffe07964aece999e6c012c56bad679b926f977
P1Za <38831183+P1Za@users.noreply.github.com>
2023-10-08 22:54:58 -0700

    finish code

---
+++ exercises/exercise1/exerices1/src/exerices1/EnglishNames.java
@@ -0,0 +1,27 @@
+/**
+ * @author Xinzhe Yuan ON 8/10/2023.
+ * @project exerices1
+ **/
+package exerices1;
+class EnglishNames {
+    static String[] nameList = {"Noah",
+            "Oliver",
+            "George",
+            "Arthur",
+            "Muhammad",
+            "Leo",
+            "Harry",
+            "Oscar" ,
+            "Archie" ,
+            "Henry","Olivia"
+            ,"Amelia",
+            "Isla",
+            "Ava",
+            "Ivy",
+            "Freya",
+            "Lily",
+            "Florence",
+            "Mia",
+            "Willow"
+    };
+}
